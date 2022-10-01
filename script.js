bac1b52ee9cc79063eb7a355d0830e34a256aaec
Roosterbear <lfpereag@gmail.com>
2022-10-01 01:33:24 -0500

    2500 time test

--- script.js
+++ script.js
@@ -58,17 +58,17 @@ function buttonEffect(itsRight,button){
     button.classList.add('rightAnswer');
     setTimeout(function(){
       button.classList.remove('rightAnswer');
-    },1500);
+    },2500);
     allRightAnswers = allRightAnswers+1;
   }else{
     button.classList.add('wrongAnswer');
     setTimeout(function(){
       button.classList.remove('wrongAnswer');
-    },1500);
+    },2500);
   }
   setTimeout(function(){
     ponerVerbo();
-  },1500);
+  },2500);
 }
 
 // First button listener
